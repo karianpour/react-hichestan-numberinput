039ec4d6f11d506c1a917621b096d91ed29e7086
Kayvan Arianpour <karianpour@gmail.com>
2018-11-18 17:37:48 +0330

    adding examples

---
+++ docs/precache-manifest.0b29a4e579f227bd2a3c58228d998ec4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c1863019f7744905e6d",
+    "url": "/static/js/main.2c186301.chunk.js"
+  },
+  {
+    "revision": "af3339f7a98c3a8669a8",
+    "url": "/static/js/1.af3339f7.chunk.js"
+  },
+  {
+    "revision": "2c1863019f7744905e6d",
+    "url": "/static/css/main.a309efdf.chunk.css"
+  },
+  {
+    "revision": "7a719850742e3275c3a67195e0598d1e",
+    "url": "/index.html"
+  }
+];
