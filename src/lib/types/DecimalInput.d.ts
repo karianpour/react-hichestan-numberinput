172af8200fb422089836d4dad05fd891aeecd02d
Mehrnoosh <mehrnoosh.akbarizadeh@gmail.com>
2021-02-12 12:59:47 +0300

    define DecimalInputProps

--- src/lib/types/DecimalInput.d.ts
+++ src/lib/types/DecimalInput.d.ts
@@ -1,3 +1,28 @@
-import * as React from 'react';
-declare const DecimalInput: React.SFC<any>;
+import * as React from "react";
+
+export type DecimalInputProps = {
+  numberFormat?: NumberFormat;
+  inputRef?: any;
+  getInputRef?: any;
+  disabled?: boolean;
+  readOnly?: boolean;
+  thousandSeparator?: string;
+  decimalSeparator?: string;
+  maxDigits?: number;
+  maxDecimal?: number;
+} & (
+  | {
+      onChange?: (event: { target: { name: string; value: string } }) => void;
+      value?: string;
+      asString?: true;
+    }
+  | {
+      onChange?: (event: { target: { name: string; value: number } }) => void;
+      value?: number;
+      asString?: false;
+    }
+) &
+  React.ComponentProps<"input">;
+
+declare const DecimalInput: React.FC<HTMLInputElementProps & DecimalInputProps>;
 export default DecimalInput;
--- src/lib/types/index.d.ts
+++ src/lib/types/index.d.ts
@@ -1,6 +1,13 @@
-import DecimalInput from './DecimalInput';
-import NumberInput from './NumberInput';
-import CardNumberInput from './CardNumberInput';
-import { NUMBER_FORMAT_FARSI, NUMBER_FORMAT_LATIN} from './util';
+import DecimalInput from "./DecimalInput";
+import NumberInput from "./NumberInput";
+import CardNumberInput from "./CardNumberInput";
+import { NUMBER_FORMAT_FARSI, NUMBER_FORMAT_LATIN, NumberFormat } from "./util";
 
-export { DecimalInput, NumberInput, CardNumberInput, NUMBER_FORMAT_FARSI, NUMBER_FORMAT_LATIN };
+export {
+  DecimalInput,
+  NumberInput,
+  CardNumberInput,
+  NUMBER_FORMAT_FARSI,
+  NUMBER_FORMAT_LATIN,
+  NumberFormat,
+};
