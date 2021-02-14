4a43f4400a78325f2b1e6a4b4a16f19c012c9de6
Mehrnoosh <mehrnoosh.akbarizadeh@gmail.com>
2021-02-14 17:46:15 +0300

    insert type to project

--- src/lib/types/CardNumberInput.d.ts
+++ src/lib/types/CardNumberInput.d.ts
@@ -1,3 +1,35 @@
-import * as React from 'react';
-declare const CardNumberInput: React.SFC<any>;
+import * as React from "react";
+declare const CardNumberInput: React.FC<
+  HTMLInputElementProps & CardNumberProps
+>;
+
+export type CardNumberProps = {
+  numberFormat?: NumberFormat;
+  inputRef: any;
+  getInputRef: any;
+  name: string;
+  style: object;
+  className: string;
+  disabled: bool;
+  readOnly: bool;
+  onClick: func;
+  onFocus: func;
+  value: string;
+} & (
+  | {
+      onChange?: (event: { target: { name: string; value: string } }) => void;
+      value?: string;
+      asString?: true;
+    }
+  | {
+      onChange?: (event: { target: { name: string; value: number } }) => void;
+      value?: number;
+      asString?: false;
+    }
+) &
+  React.ComponentProps<"input">;
+
+declare const CardNumberInput: React.FC<
+  HTMLInputElementProps & CardNumberProps
+>;
 export default CardNumberInput;
